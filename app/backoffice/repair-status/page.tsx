6e0c26db318b6ba41da6d00ec90e8b2b6c65fc8c
thekaroe-thailand <thekaroe@gmail.com>
2024-12-19 21:25:39 +0700

    status repair

--- app/backoffice/repair-status/page.tsx
+++ app/backoffice/repair-status/page.tsx
@@ -11,15 +11,45 @@ export default function Page() {
     const [repairRecords, setRepairRecords] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [id, setId] = useState(0);
+    const [status, setStatus] = useState('');
+    const [solving, setSolving] = useState('');
+    const [statusList, setStatusList] = useState([
+        { value: 'active', label: 'รอซ่อม' },
+        { value: 'pending', label: 'รอลูกค้ายืนยัน' },
+        { value: 'repairing', label: 'กำลังซ่อม' },
+        { value: 'done', label: 'ซ่อมเสร็จ' },
+        { value: 'cancel', label: 'ยกเลิก' },
+        { value: 'complete', label: 'ลูกค้ามารับอุปกรณ์' },
+    ]);
+    const [statusForFilter, setStatusForFilter] = useState('');
+    const [tempRepairRecords, setTempRepairRecords] = useState([]);
+    const [engineers, setEngineers] = useState([]);
+    const [engineerId, setEngineerId] = useState(0);
 
     useEffect(() => {
         fetchRepairRecords();
+        fetchEngineers();
     }, []);
+
+    const fetchEngineers = async () => {
+        try {
+            const response = await axios.get(`${config.apiUrl}/api/user/listEngineer`);
+            setEngineers(response.data);
+            setEngineerId(response.data[0].id);
+        } catch (error: any) {
+            Swal.fire({
+                icon: 'error',
+                title: 'error',
+                text: error.message,
+            });
+        }
+    }
 
     const fetchRepairRecords = async () => {
         try {
             const response = await axios.get(`${config.apiUrl}/api/repairRecord/list`);
             setRepairRecords(response.data);
+            setTempRepairRecords(response.data);
         } catch (error: any) {
             Swal.fire({
                 icon: 'error',
@@ -30,33 +60,65 @@ export default function Page() {
     }
 
     const getStatusName = (status: string) => {
-        switch (status) {
-            case 'active':
-                return 'รอซ่อม';
-            case 'pending':
-                return 'รอลูกค้ายืนยัน';
-            case 'repairing':
-                return 'กำลังซ่อม';
-            case 'done':
-                return 'ซ่อมเสร็จ';
-            case 'cancel':
-                return 'ยกเลิก';
-            case 'complete':
-                return 'ลูกค้ามารับอุปกรณ์';
-            default:
-                return 'รอซ่อม';
-        }
+        const statusObj = statusList.find((item: any) => item.value === status);
+        return statusObj?.label ?? 'รอซ่อม';
     }
 
     const handleEdit = (id: number) => {
-        setId(id);
-        setShowModal(true);
+        const repairRecord = repairRecords.find((repairRecord: any) => repairRecord.id === id) as any;
+
+        if (repairRecord) {
+            setId(id);
+            setStatus(repairRecord?.status ?? '');
+            setSolving(repairRecord?.solving ?? '');
+            setShowModal(true);
+        }
+    }
+
+    const handleSave = async () => {
+        try {
+            const payload = {
+                status: status,
+                solving: solving,
+                engineerId: engineerId
+            }
+
+            await axios.put(`${config.apiUrl}/api/repairRecord/updateStatus/${id}`, payload);
+            fetchRepairRecords();
+            setShowModal(false);
+        } catch (error: any) {
+            Swal.fire({ icon: 'error', title: 'error', text: error.message });
+        }
+    }
+
+    const handleFilter = (statusForFilter: string) => {
+        if (statusForFilter) {
+            const filteredRecords = tempRepairRecords.filter((repairRecord: any) => repairRecord.status === statusForFilter);
+            setRepairRecords(filteredRecords);
+            setStatusForFilter(statusForFilter);
+        } else {
+            setRepairRecords(tempRepairRecords);
+            setStatusForFilter('');
+        }
     }
 
     return (
         <>
             <div className="card">
                 <h1>สถานะการซ่อม</h1>
+                <div>
+                    <select className="form-control"
+                        value={statusForFilter}
+                        onChange={(e) => handleFilter(e.target.value)}
+                    >
+                        <option value="">--- ทั้งหมด ---</option>
+                        {statusList.map((item: any) => (
+                            <option value={item.value} key={item.value}>
+                                {item.label}
+                            </option>
+                        ))}
+                    </select>
+                </div>
                 <div className="card-body">
                     <table className="table mt-3">
                         <thead>
@@ -96,24 +158,47 @@ export default function Page() {
 
             <Modal title="ปรับสถานะ" isOpen={showModal} onClose={() => setShowModal(false)}>
                 <div>
-                    <div>เลือกสถานะ</div>
-                    <div>
-                        <select className="form-control w-full">
-                            <option value="active">รอซ่อม</option>
-                            <option value="pending">รอลูกค้ายืนยัน</option>
-                            <option value="repairing">กำลังซ่อม</option>
-                            <option value="done">ซ่อมเสร็จ</option>
-                            <option value="cancel">ยกเลิก</option>
-                            <option value="complete">ลูกค้ามารับอุปกรณ์</option>
-                        </select>
+                    <div className="flex gap-4">
+                        <div className="w-1/2">
+                            <div>เลือกสถานะ</div>
+                            <div>
+                                <select className="form-control w-full"
+                                    value={status}
+                                    onChange={(e) => setStatus(e.target.value)}
+                                >
+                                    {statusList.map((item: any) => (
+                                        <option value={item.value} key={item.value}>
+                                            {item.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+                        <div className="w-1/2">
+                            <div>เลือกช่างซ่อม</div>
+                            <div>
+                                <select className="form-control w-full"
+                                    value={engineerId}
+                                    onChange={(e) => setEngineerId(Number(e.target.value))}
+                                >
+                                    {engineers.map((engineer: any) => (
+                                        <option value={engineer.id} key={engineer.id}>
+                                            {engineer.username}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="mt-3">
                         <div>การแก้ไข</div>
-                        <textarea className="form-control w-full" rows={5}></textarea>
+                        <textarea className="form-control w-full" rows={5}
+                            value={solving}
+                            onChange={(e) => setSolving(e.target.value)}></textarea>
                     </div>
 
-                    <button className="btn-primary mt-3">
+                    <button className="btn-primary mt-3" onClick={handleSave}>
                         <i className="fa-solid fa-check mr-3"></i>
                         บันทึก
                     </button>
